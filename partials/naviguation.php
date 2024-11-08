7ab136cb9ef5a865d062810f5ef54f7b4f30d72f
Simon Bouchard <simon.bouchard31@gmail.com>
2024-11-07 21:32:48 -0500

    Change urls

--- partials/naviguation.php
+++ partials/naviguation.php
@@ -1,7 +1,7 @@
 <nav>
 	<ul class=nav>
 		<li><a href="/">Home</a></li>
-		<li><a href="ExerciseSolution">Exercises</a></li>
+		<li><a href="ExerciseSolutions">Exercises</a></li>
 		<li><a href="About">About</a></li>
 	</ul>
 </nav>
